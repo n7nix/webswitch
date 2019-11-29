fb8c15964424ea98691b4eda1aedfb468f3c013b
n7nix <basil@pacabunga.com>
2019-11-29 13:36:43 -0800

    Parameterize state & mode cut values for the gpio readall command.

--- dli/iot_power_relay/gpiodli_relay.sh
+++ dli/iot_power_relay/gpiodli_relay.sh
@@ -4,9 +4,28 @@
 #  BCM  wPi      cut
 #  16  gpio.27   11
 #  17  gpio. 0    5
+#
+# If HD-15 connector is not used then these gpios are available:
+#  BCM   wPi    hdr
+#  22  gpio. 3  15
+#  23  gpio. 4  16
+#  24  gpio. 5  18
+#  27  gpio. 2  13
 
 gpio_num="17"
 wpi_num="gpio. 0"
+gpio_mode_cut=5
+gpio_state_cut=6
+
+gpio_num="27"
+wpi_num="gpio. 2"
+gpio_mode_cut=5
+gpio_state_cut=6
+
+#gpio_num=16
+#wpi_num=gpio.27
+#gpio_mode_cut=11
+#gpio_state_cut=10
 
 scriptname="`basename $0`"
 
@@ -17,7 +36,7 @@ function usage() {
 }
 
 # ===== main
-mode_gpio="$(gpio readall | grep -i "$wpi_num" | cut -d "|" -f 5 | tr -d '[:space:]')"
+mode_gpio="$(gpio readall | grep -i "$wpi_num" | cut -d "|" -f $gpio_mode_cut | tr -d '[:space:]')"
 
 if [ "$mode_gpio" != "OUT" ] ; then
    echo "gpio $gpio_num is in wrong mode: |$mode_gpio|, should be: OUT"
@@ -37,9 +56,10 @@ case $arg in
       usage
       ;;
    *)
-      state_gpio="$(gpio readall | grep -i "$wpi_num" | cut -d "|" -f 6 | tr -d '[:space:]')"
+      state_gpio="$(gpio readall | grep -i "$wpi_num" | cut -d "|" -f $gpio_state_cut | tr -d '[:space:]')"
+#      echo "debug: gpio state: $state_gpio"
       state_str="off"
-      if [ "$state_gpio" -eq "1" ] ; then
+      if [ "$state_gpio" == "1" ] ; then
          state_str="on"
       fi
       echo "gpio $gpio_num is $state_str"
